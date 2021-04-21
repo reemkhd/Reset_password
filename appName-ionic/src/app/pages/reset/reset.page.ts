63c1e19d93f3b734ef1db407681ca97522a728b4
reemkhd <reemnote@gmail.com>
2021-04-21 11:17:52 +0300

    add files

---
+++ appName-ionic/src/app/pages/reset/reset.page.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController, NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/services/alert.service';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-reset',
+  templateUrl: './reset.page.html',
+  styleUrls: ['./reset.page.scss'],
+})
+export class ResetPage implements OnInit {
+
+  constructor(
+    private modalController: ModalController,
+    private navCtrl: NavController,
+    private route: Router,
+    private alertService: AlertService,
+    private authService: AuthService,
+  ) { }
+  ngOnInit() {
+  }
+  dismissReset() {
+    this.modalController.dismiss();
+  }
+  reset(form: NgForm) {
+    this.authService.reset(form.value.email).subscribe(
+      data => {
+        this.alertService.presentToast("Send password link");
+      },
+      error => {
+        console.log(error);
+        this.alertService.presentToast("error")
+      },
+      () => {
+        this.dismissReset();
+        this.navCtrl.navigateRoot('/dashboard');
+      }
+    );
+  }
+}
